76f4a893d37802e4d66d54e7dcdc74ae78c3e46c
saeed <alsaeedalbasi0ny@gmail.com>
2024-11-23 00:49:00 +0200

    did some changes that make main page almost done

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-let menlist = document.getElementById("menuList");
+// let menlist = document.getElementById("menuList");
 
-menuList.style.display = "none";
+// menuList.style.display = "none";
 
-function toggleMenue() {
-    if (menuList.style.display == "none") 
-    {
-        menuList.style.display = "block";
-    }else 
-    {
-        menuList.style.display = "none";
-    }
-}
+// function toggleMenue() {
+//     if (menuList.style.display == "none") 
+//     {
+//         menuList.style.display = "block";
+//     }else 
+//     {
+//         menuList.style.display = "none";
+//     }
+// }
